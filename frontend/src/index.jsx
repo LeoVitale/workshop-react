a9000969ab792ffe46781427a738a62eadc370ed
Leonardo Vitale <leonardo_vitale@hotmail.com>
2017-03-10 01:40:46 -0300

    Added Hot Reloading

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,1 +1,20 @@
-console.log('teste');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './components/app';
+
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'));
+};
+
+render(App);
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./components/app', () => { render(App) })
+}
