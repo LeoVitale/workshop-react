42a71c61e40e5787a9ffada90d1ed15787e7cc1e
Leonardo Vitale <leonardo_vitale@hotmail.com>
2017-03-10 02:11:39 -0300

    Created Header Layout

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import AppRoutes from './routes';
+import Menu from './common/menu';
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        a
-        <AppRoutes/>
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <Menu />
+        <main className="mdl-layout__content">
+          <div className="page-content">
+            <AppRoutes />
+          </div>
+        </main>
+        
       </div>
     );
   }
