0c5dab1260a112fa0bc588e9e728d49369534c3a
Leonardo Vitale <leonardo_vitale@hotmail.com>
2017-03-10 01:55:15 -0300

    Created Routing

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import AppRoutes from './routes';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        Appssssssssss
+        a
+        <AppRoutes/>
       </div>
     );
   }
